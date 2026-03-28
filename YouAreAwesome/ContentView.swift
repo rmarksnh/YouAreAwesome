7c86c91ac567058ac63dbd64a17b51f6a0af1d25
Rob <robmarks@comcast.net>
2026-03-28 08:31:51 -0400

    After adding Spacers and Frame

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,18 +11,31 @@ struct ContentView: View {
     @State private var message = "I Am A Programmer!"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
             Text(message)
                 .font(.largeTitle)
-                .foregroundStyle(.red)
-                .padding()
-            Button("Click me!") {
-                message = "Awesome!"
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                              
+                Button("Great!") {
+                    message = "Great!"
+                }
             }
-
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
