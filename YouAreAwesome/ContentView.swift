dbb5cdf223c0d49d7f4b774e959077a897817f49
Rob <robmarks@comcast.net>
2026-04-01 10:38:20 -0400

    After adding .minimumScaleFactor and .animation

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,29 +10,46 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
-        
+        let messages: [String] = ["You're Awesome",
+                                  "When the Genius Bar Needs Help, They Call You!",
+                                  "Gadzooks my friend, I am astonished at how utterly magnificent you are",
+                                 "You're Great",
+                                 "You're Fantastic",
+                                 "You're Incredible",
+                                 "You're Amazing",
+                                 "You're Super"]
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+            
             
             Spacer()
             
-            Button("Press Me!") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
-                
-                message = (message == message1 ? message2 : message1)
-                imageName = (imageName == "image0" ? "image1" : "image0")
+            Button("Show Message") {
+
+                if !messages.isEmpty {
+                    message = messages[messageNumber]
+                    messageNumber = (messageNumber == (messages.count - 1) ? 0 : messageNumber + 1)
+                }
+                imageName = "image\(imageNumber)"
+                imageNumber = (imageNumber == 9 ? 0 : imageNumber + 1)
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
